19e8b63cd12467c18187c53ec61b1b36dedec1ad
Farseen Shaikh <farseenshaikh20@gmail.com>
2026-04-12 10:35:32 +0530

    fix: ignore optional wagmi connector deps for Vercel build

--- apps/studio/next.config.ts
+++ apps/studio/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@earnforge/sdk'],
+  webpack: (config, { webpack }) => {
+    // wagmi connectors reference optional peer deps for connectors we don't use.
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^(@walletconnect\/ethereum-provider|@coinbase\/wallet-sdk|@metamask\/connect-evm|@base-org\/account|@safe-global\/safe-apps-sdk|@safe-global\/safe-apps-provider|porto|porto\/internal|bs58)$/,
+      }),
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
